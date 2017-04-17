31db5adf4d34735bbc264e5485f3c632eecde6e9
Edith Mandujano <edithmandujano001@gmail.com>
2017-04-17 12:05:27 -0500

    Applove Parte2 Intento1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
 //alert("ya estoy enlazado")
+function miVentana1(){
+    var ventana = document.getElementById("miVentana1");
+    ventana.style.marginTop = "100px";
+    ventana.style.marginLeft = auto;
+    ventana.style.marginRight= auto;
+    ventana.style.display = "block";
+}
+console.log(miVentana1);
